368abf7931775596b3fa646dd387b0cf7c20ec68
Elen Bhattarai <elen@Elens-MacBook-Air.local>
2022-04-24 11:34:54 -0500

    finished the test suite

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ let ingredients = []
 let modal = document.querySelector('.modall')
 let modalContent = document.querySelector('.modall-content')
 
+//insertion Sort
 function insertionSort(arr)
 {
     let i, key, j;
@@ -204,93 +205,147 @@ const createResultCards = () => {
 
 //remove the cards that were already there from the previous result
 const removeResultCards = () => {
-    let resultCards = Array.from(document.getElementsByClassName('cards'))  
+    //grab resultCards from the DOM
+    let resultCards = Array.from(document.getElementsByClassName('cards')) 
+    //run a for loop for all the resultCards 
     for(let i = 0 ; i < resultCards.length; i++) {
+        //select the ith result Card
         let a = resultCards[i]
+        //remove it
         a.remove()
     }
 }   
 
+//view the Recipes of the query dishes
 const viewRecipe = (name,title,image, usedIngredients, missedIngredients) => {
+    //view the modal
     modal.style.display = "block"
+    //make a button to close the modal
     let buttonToCloseModal = document.querySelector('.toClose')
     let ab = modalContent.children[0]
+    //add an event listener to close the button Modal
     buttonToCloseModal.addEventListener("click", ()=> {
         modal.style.display = "none"
         modalContent.innerHTML = '<button class = "toClose">Close</button>'
     })
+    //fetch the Recipe
     fetchRecipe(name,title,image,usedIngredients, missedIngredients)
 }
 
+//this function fetches the recipes for us
 const fetchRecipe = async (rname,title, image, usedIngredients, missedIngredients) => {
     let rnameInt = Number(rname)
+    //fetch the recipes from the API
     let rdata = await fetch(`https://api.spoonacular.com/recipes/${rnameInt}/analyzedInstructions?apiKey=112c9e46de254a86a707eb74c737b3a1`)
+    //convert the data that we got into json
     let rjson = await rdata.json()
     // console.log(rjson)
+    //add the data to the modal
     addDatatoModal(rjson,title,image,usedIngredients, missedIngredients)
 }
 
+//function to download specific recipes locally
+//throws an exception if the download cannot happen
 const downloadF = (filename, text) => {
+    let downloaded = false;
+    //create an href and name it element
     var element = document.createElement('a');
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
     element.setAttribute('download', filename);
-  
+    //don't display the element on the DOM
     element.style.display = 'none';
+    //append the element to the DOM body
     document.body.appendChild(element);
-  
+    
+    //click the element href that we created
     element.click();
   
     document.body.removeChild(element);
+    downloaded = true;
+    //if the download isn't sucessful
+    if(downloaded = false){
+        //throw an exception saying the file couldn't be downloaded
+        throw new Error("The file couldn't be downloaded")
+    }
 }
 
+//After an empty modal is created,this function adds the steps to make the recipe into the modal
 const addDatatoModal = (rjson,title,image,usedIngredients, missedIngredients) => {
-
+    //create an h2 to make the title
     let rname = document.createElement("h2")
+    //set the title to be the innerText
     rname.innerText = title
+    //add the title to the modal
     modalContent.appendChild(rname)
-
+    //create an image tag
     let rimg = document.createElement("img")
+    //set the image src that we got from the previous API result
     rimg.src = image
+    //set the width of the image
     rimg.style.width = "200px"
+    //set the height of the image
     rimg.style.height = "200px"
-
+    //add the image to the modal
     modalContent.appendChild(rimg)
 
     let allIngredients = []
 
+    //loop through all the usedIngredients
     for(let i = 0; i < usedIngredients.length; i++) {
+        //add it into the all ingredients list
         allIngredients.push(usedIngredients[i].name)
     }
 
+    //loop through all the missed Ingredients list
     for(let i = 0; i < missedIngredients.length; i++) {
+        //add it to the missedIngredients list
         allIngredients.push(missedIngredients[i].name)
     }
 
+    //crete a div
     let ingredients = document.createElement("div")
+    //give it a classname called ingredients to apply CSS
     ingredients.classList.add("ingredients")
+    //set a innerHTML to the ingredients
     ingredients.innerHTML = 'Ingredients: '
 
-
+    //loop through all the elements on the allIngredients array
     for(let i = 0; i < allIngredients.length; i++) {
+        //add all of them to the ingredients div innerText
         ingredients.innerText += allIngredients[i] + ", "
     }
-
+    //append all the ingredients to the modal
     modalContent.appendChild(ingredients)
 
+    //create a div to add in the steps to make the recipe
     let steps = document.createElement("div")
+    //add a class of steps to apply CSS to it
     steps.classList.add("steps")
+    //add the steps to the modal
     modalContent.appendChild(steps)
 
+    //loop through all the steps that are given by the API
     for(let i = 0; i < rjson[0].steps.length; i++) {
+        //make a div
         let div = document.createElement("div")
+        //add the class of steps Text to apply CSS
         div.classList.add("stepsText")
+        //in the div, add the steps given from the API
         div.innerText = i+1 + ") " + rjson[0].steps[i].step + "\n"
+        //append the divs to the steps div that we created
         steps.appendChild(div)
     }
 
+    //create a download button
     let download = document.createElement("button")
+    //add a class of download to it to apply CSS
     download.classList.add('download')
+    //set the inner text of the button
     download.innerText = "Download Recipe"
+    //when the button is clicked, the function downloadF will be called
     download.addEventListener("click", ()=> downloadF(`${rname.innerText} recipe.txt`, rname.innerText + "\n\n" + ingredients.innerText + "\n\n"+ steps.innerText))
+    //add the download button to the modal
     modalContent.appendChild(download)
 }
+
+
